91ba51d969a56f03da98840514a695bc4528e7f6
Kyle Nguyen <knguyen1193@outlook.com>
2023-12-20 11:04:21 -0800

    add all file

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import Cell from "./Cell";
 
 const Board = ({ board, onCellClick }) => {
+    /*
+        1. JSX Context: the 'return' statement is returning JSX, which is HTML-like syntax used in React components
+        2. Javascript expression: inside this JSX, you want to run a Javascript expression
+    */
     return (
         <div className='grid grid-cols-3 gap4'>
-            {board.map((value, index) =>(
+            {board.map((value, index) => (
                 <Cell key={index} value={value} onClick={() => onCellClick(index)} />
             ))}
-        </div>   
+        </div>
     );
 };
 
