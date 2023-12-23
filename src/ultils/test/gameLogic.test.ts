be761c9dc89544e19ba92fc9c628ce823181f368
Kyle Nguyen <knguyen1193@outlook.com>
2023-12-23 13:44:07 -0800

    add all file

--- src/ultils/test/gameLogic.test.ts
+++ src/ultils/test/gameLogic.test.ts
@@ -8,4 +8,29 @@ describe('Game Logic Tests' , () => {
         expect(initialState.currentPlayer).toBe(Player.X)
         expect(initialState.scores).toEqual({ [Player.X]: 0, [Player.O]: 0})
     });
+
+    test('makeMove shoud not update the board for invalid move', () =>{
+        const initialState = initializeGame();
+        const newState = makeMove(initialState, 0);
+        const finalState = makeMove(newState, 0);
+        expect(finalState).toEqual(newState);
+    });
+
+    test('makeMove shoud alternate players', () =>{
+
+    });
+
+
+    test('makeMove should update the board correctly', ()=>{
+        const initialState = initializeGame();
+        const newState = makeMove(initialState,0);
+        expect(newState.board[0]).toBe(Player.X);
+    });
+
+    test('checkWiner should correctly identify the winner', ()=>{
+        const winningBoard = [Player.X, Player.X, Player.X, null, null, null, null, null, null];
+        expect(checkWinner(winningBoard)).toBe(Player.X);    
+    });
 })
+
+
